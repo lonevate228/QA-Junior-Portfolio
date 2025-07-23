593cbe01a83171c67b3fb4c6e450ce9a75c5846a
lonevate228 <lonevate@gmail.com>
2025-07-23 14:53:20 +0500

    Update SQL_Queriesselect_users.sql

--- SQL_Queriesselect_users.sql
+++ SQL_Queriesselect_users.sql
@@ -1,1 +1,2 @@
-SELECT name, age FROM users WHERE age > 18;
+SELECT name, age FROM users WHERE age > 18; // Найти всех пользователей старше 18 лет
+SELECT user_id, COUNT(*) FROM orders GROUP BY user_id; // Найти кол-во заказов по каждому пользователю
